df3e8263a3e6a6d3ce5b42c11727b1a6a8cd8107
Dy4nk <aliermarron32@gmail.com>
2023-04-25 11:28:37 +0200

    Optimized the code and made a better README

--- pip_package_updater.py
+++ pip_package_updater.py
@@ -4,17 +4,22 @@
 # LICENSE: MIT
 # INFO: Simple python script to update all installed pip packages
 
-from subprocess import run, call, PIPE
-import os
+import sys
+import subprocess
+
+# Determine the operating system
+if sys.platform.startswith("win"):
+    newline = "\r\n"
+else:
+    newline = "\n"
 
 print("Tool for updating installed pip packages - v0.1")
-match os.name:
-    case "nt":
-        result: str = str(run("pip freeze", stdout=PIPE).stdout)[2:-2]
-        packages: list[str] = [package_version[:package_version.index("=")] for package_version in result.split("\\r\\n")]
-        call(f"python.exe -m pip install --upgrade {' '.join(packages)}")
-    case "posix":
-        result: str = str(run(["python3", "-m","pip", "freeze"], stdout=PIPE).stdout)[2:-2]
-        packages: list[str] = [package_version[:package_version.index("=")] for package_version in result.split("\\n")]
-        command: list[str] = ["python3", "-m", "pip", "install", "--upgrade"] + packages
-        call(command)
+
+# List outdated packages
+output = subprocess.check_output(["pip", "list", "--outdated"]).decode()
+
+# Extract package names
+packages = [line.split()[0] for line in output.split(newline)[2:-1]]
+
+# Update packages
+subprocess.check_call(["pip", "install", "--upgrade"] + packages)
